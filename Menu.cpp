15e331d62a4ee3ed9f3515f37707517819d40183
exercsie <hammondtristan62@gmail.com>
2026-03-03 15:13:55 +1100

    Created menu start

---
+++ Menu.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    while(true) {
+        std::cout << "Welcome to asymmetric cryptography!\n";
+        std::cout << "1 - DSA verification\n";
+        std::cout << "2 - RSA verification\n";
+        std::cout << "3 - Info\n";
+
+
+        int choice;
+        std::cout << "Choose an option: ";
+        std::cin >> choice;
+        if(std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            std::cout << "\nInvalid input. Try again!\n\n";
+            continue;
+        } else if (choice < 1 || choice > 3) {
+            std::cout << "\nInvalid choice. Try again!\n\n";
+        }
+
+        switch(choice) {
+            
+        }
+    }
+}
