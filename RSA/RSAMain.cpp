fb611d75513328f34e96e3e721b932a19c4795e2
exercsie <hammondtristan62@gmail.com>
2026-03-03 13:53:09 +1100

    Basic variable entry

--- RSA/RSAMain.cpp
+++ RSA/RSAMain.cpp
@@ -3,5 +3,16 @@
 #include "../ModularInverse.h"
 
 int main() {
-    
+    int p, q, e, m, n;
+    std::cout << "Welcome to RSA verifier!\n";
+    std::cout << "Please input first prime number, p: ";
+    std::cin >> p;
+    std::cout << "Please input sencond prime number, q: ";
+    std::cin >> q;
+    std::cout << "Please input public key, e: ";
+    std::cin >> e;
+    std::cout << "Please input plain text number, m: ";
+    std::cin >> m;
+    std::cout << "Please input modulus, n: ";
+    std::cin >> n;
 }
