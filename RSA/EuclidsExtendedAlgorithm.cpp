5f801aebbc8c08aad15e636d69b296e45233121f
exercsie <hammondtristan62@gmail.com>
2026-03-05 14:45:14 +1100

    Error handling if e and phiN are not coprime

--- RSA/EuclidsExtendedAlgorithm.cpp
+++ RSA/EuclidsExtendedAlgorithm.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <numeric>
 #include "../ModularExponentiation.h"
 #include "ModularSecondNumPart.h"
 
 int miniEuclidsExtendedAlgorithm(int e, int phiN) {
+    if(std::gcd(e, phiN) != 1) {
+        std::cout << "e and phiN are not coprime, therefore modular inverse does not exist.\n";
+        return 1; 
+    }
+    
     int x = getModularPart(phiN, e); // 40 / 3 = 13.33, 40 - 13 * 3 = 1
     int d = phiN + x; // 40 + -13 = 27
 
